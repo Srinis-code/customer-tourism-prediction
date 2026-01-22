869ebf1fb5da5c2abf0e0570cdd51e3e8d903795
Srinis-code <ksricheenu@gmail.com>
2026-01-22 11:52:11 +0530

    Update data_register.py

--- visit-with-us/model_building/data_register.py
+++ visit-with-us/model_building/data_register.py
@@ -1,28 +1,33 @@
-from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+from huggingface_hub.utils import RepositoryNotFoundError
 from huggingface_hub import HfApi, create_repo
 import os
 
 # Initialize API client
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
-repo_id="ksricheenu/customer-tourism-prediction-dataset"
-repo_type="dataset"
+repo_id = "ksricheenu/customer-tourism-prediction-dataset"
+repo_type = "dataset"
 
-# Step 1: Check if the space exists
+# Step 1: Check if the dataset repo exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Space '{repo_id}' already exists. Using it.")
+    print(f"Dataset '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
-    print(f"Space '{repo_id}' not found. Creating new space...")
+    print(f"Dataset '{repo_id}' not found. Creating it...")
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Space '{repo_id}' created.")
+    print(f"Dataset '{repo_id}' created.")
 
-# Path relative to the repo root in GitHub Actions
+# Path relative to GitHub repo root
 folder_path = "visit-with-us/data"
 
 # Upload the folder
 api.upload_folder(
     folder_path=folder_path,
-    repo_id="ksricheenu/customer-tourism-prediction-dataset"
-    repo_type="dataset"
+    repo_id=repo_id,
+    repo_type=repo_type,
 )
+
+print("Dataset upload completed successfully.")
+
+
+
